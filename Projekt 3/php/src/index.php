d0de5db17735a05436e30b4b4c989e75118c61a5
Pmk28 <127778553+Pmk28@users.noreply.github.com>
2024-06-16 21:27:53 +0200

    Add files via upload

---
+++ Projekt 3/php/src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <ul>
+        <li><a href='./xsd-validate.php'>XSD validátor</a></li>
+    </ul>
+    <ul>
+        <li><a href='./soubory.php'>Soubory</a></li>
+    </ul>
+</body>
+
+</html>
